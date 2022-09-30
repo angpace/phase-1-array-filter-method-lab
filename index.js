2ef723481ff92275a8863b4ae586fedc7683e3e7
Angela Pace <angpace13@gmail.com>
2022-09-30 14:35:48 -0400

    commit tree 6bc83698fe19784d34e0dae258b44e880e930ff4

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solution here
+function findMatching(name) {
+   const newArray = filter(name["name"] === "Bobby")
+   if (newArray) { 
+    return name
+   }
+  }
+
+    // 2) returns matching drivers if case does not match but letters do
+     // 3) returns an empty array if there is no match
+
+//unction filter(collection, cb) {
+  //  for (const user of collection) {
+   //     if (cb(user)) {
+     //     console.log(user.firstName);
